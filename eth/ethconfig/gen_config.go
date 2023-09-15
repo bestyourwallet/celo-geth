6a3fd97ba78396b13ce8222d41ef01a880b443d9
Paul Lange <palango@gmx.de>
2023-09-15 14:06:04 +0200

    configs: Add Cel2 hardfork
    This will be the hardfork used to enable all Celo
    related features in the op-geth.
    
    Disables Cel2 fork for certain tests
    
    Note that the bedrock block is the cel2 block

--- eth/ethconfig/gen_config.go
+++ eth/ethconfig/gen_config.go
@@ -57,6 +57,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 		OverrideOsaka                             *uint64 `toml:",omitempty"`
 		OverrideVerkle                            *uint64 `toml:",omitempty"`
 		OverrideOptimismCanyon                    *uint64 `toml:",omitempty"`
+		OverrideOptimismCel2                      *uint64 `toml:",omitempty"`
 		OverrideOptimismEcotone                   *uint64 `toml:",omitempty"`
 		OverrideOptimismFjord                     *uint64 `toml:",omitempty"`
 		OverrideOptimismGranite                   *uint64 `toml:",omitempty"`
@@ -117,6 +118,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 	enc.OverrideOsaka = c.OverrideOsaka
 	enc.OverrideVerkle = c.OverrideVerkle
 	enc.OverrideOptimismCanyon = c.OverrideOptimismCanyon
+	enc.OverrideOptimismCel2 = c.OverrideOptimismCel2
 	enc.OverrideOptimismEcotone = c.OverrideOptimismEcotone
 	enc.OverrideOptimismFjord = c.OverrideOptimismFjord
 	enc.OverrideOptimismGranite = c.OverrideOptimismGranite
@@ -181,6 +183,7 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 		OverrideOsaka                             *uint64 `toml:",omitempty"`
 		OverrideVerkle                            *uint64 `toml:",omitempty"`
 		OverrideOptimismCanyon                    *uint64 `toml:",omitempty"`
+		OverrideOptimismCel2                      *uint64 `toml:",omitempty"`
 		OverrideOptimismEcotone                   *uint64 `toml:",omitempty"`
 		OverrideOptimismFjord                     *uint64 `toml:",omitempty"`
 		OverrideOptimismGranite                   *uint64 `toml:",omitempty"`
@@ -323,6 +326,9 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 	}
 	if dec.OverrideOptimismCanyon != nil {
 		c.OverrideOptimismCanyon = dec.OverrideOptimismCanyon
+	}
+	if dec.OverrideOptimismCel2 != nil {
+		c.OverrideOptimismCel2 = dec.OverrideOptimismCel2
 	}
 	if dec.OverrideOptimismEcotone != nil {
 		c.OverrideOptimismEcotone = dec.OverrideOptimismEcotone
