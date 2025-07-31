celo-org/op-geth

Handle migrated celo headers (#143, #152, #228, #300,#301, #327)
This PR updates the header encoding and decoding for celo blocks to be
able to support data migrated from celo-blockchain.

Our post gingerbread headers actually decode into the op-geth header
structure without any changes. Since adding fields to the end of a
struct does not cause rlp decoding to fail for previously encoded
values. Our pre-gingerbread don't decode into the op-geth header
structure because the pre-gingerbread headers miss fields from the
middle of the struct so we need to handle those explicitly.

The decision on how to decode is made based on introspecting the rlp and
looking at the second field which is a hash (length 32 bytes)
post-gingerbread, but an address (length 20 bytes) pre-gingerbread.
Decoded headers are marked with a non exported field indicating if they
are gingerbread.

The decision on how to encode is made based on the value of the internal
field.

Pre-gingerbread blocks with no difficulty set resulted in decoded
headers with a nil difficulty, this caused problems since the codebase
assumes a non nil difficulty, so we now ensure non nil zero difficulty
for these headers.

Updated validation code to allow invalid(empty) uncle hash
values for pre-gingerbread blocks.

Allowed for genesis blocks in a CeL2 migrated chain context that lack
GasLimit and Difficulty because those fields were considered required by
op-geth but not present in early Celo.

Add special casing in `toBlockWithRoot` and to prevent default setting
of `uncleHash` inside `NewBlock` when operating in the context of a
migrated chain. Also if the genesis block is a pre-gingerbread block it
is marked as such so that it is encoded correctly.

`TestFeeHistory`, initialises a pre-gingerbread genesis but expects it
to have the new block format, so to account for that an extra condition
was added in `toBlockWithRoot` to only use the old format for
pre-gingerbread blocks that are being initialised as a result of
exe

core/celo_genesis_test.go
@@ -1,0 +1,82 @@
+package core
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToBlockWithRoot(t *testing.T) {
+	nonMigrated := &Genesis{Config: &params.ChainConfig{}}
+	var cel2Time uint64 = 1
+	migrated := &Genesis{
+		Config: &params.ChainConfig{
+			Cel2Time: &cel2Time,
+		},
+	}
+
+	t.Run("PreGingerbreadBlocksProducedWhenInitingGenesis", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				GingerbreadBlock: big.NewInt(1),
+			},
+			initingGenesis: true,
+		}
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.True(t, b.Header().IsPreGingerbread())
+	})
+	t.Run("PreGingerbreadBlocksNotProducedWhenNotInitingGenesis", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				GingerbreadBlock: big.NewInt(1),
+			},
+			initingGenesis: false,
+		}
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.False(t, b.Header().IsPreGingerbread())
+	})
+	t.Run("PreGingerbreadBlocksNotProducedWhenNotPreGingerbread", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				GingerbreadBlock: big.NewInt(0),
+			},
+		}
+		g.initingGenesis = false
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.False(t, b.Header().IsPreGingerbread())
+		g.initingGenesis = true
+		b = g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.False(t, b.Header().IsPreGingerbread())
+	})
+	t.Run("NonMigratedChainHasDefaultGasLimit&DifficultySet", func(t *testing.T) {
+		b := nonMigrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, params.GenesisGasLimit, b.Header().GasLimit)
+		assert.Equal(t, params.GenesisDifficulty, b.Header().Difficulty)
+	})
+	t.Run("MigratedChainGasLimitNotSet", func(t *testing.T) {
+		b := migrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, uint64(0), b.Header().GasLimit)
+	})
+	t.Run("MigratedChainNilDifficultyInitialisedToZero", func(t *testing.T) {
+		b := migrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, big.NewInt(0), b.Header().Difficulty)
+	})
+	t.Run("NonMigratedChainHasEmptyUncleHashSet", func(t *testing.T) {
+		b := nonMigrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, types.EmptyUncleHash, b.Header().UncleHash)
+	})
+	t.Run("PreGingerbreadBlocksOnMigratedChainsDoNotHaveEmptyUncleHashSet", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				Cel2Time:         &cel2Time,
+				GingerbreadBlock: big.NewInt(1),
+			},
+		}
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, common.Hash{}, b.Header().UncleHash)
+	})
+}

bestyourwallet/celo-geth

core/celo_genesis_test.go
@@ -1,0 +1,88 @@
+package core
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToBlockWithRoot(t *testing.T) {
+	nonMigrated := &Genesis{Config: &params.ChainConfig{}}
+	var cel2Time uint64 = 1
+	migrated := &Genesis{
+		Config: &params.ChainConfig{
+			Cel2Time: &cel2Time,
+		},
+	}
+
+	t.Run("PreGingerbreadBlocksProducedWhenInitingGenesis", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				GingerbreadBlock: big.NewInt(1),
+			},
+			initingGenesis: true,
+		}
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.True(t, b.Header().IsPreGingerbread())
+	})
+	t.Run("PreGingerbreadBlocksNotProducedWhenNotInitingGenesis", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				GingerbreadBlock: big.NewInt(1),
+			},
+			initingGenesis: false,
+		}
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.False(t, b.Header().IsPreGingerbread())
+	})
+	t.Run("PreGingerbreadBlocksNotProducedWhenNotPreGingerbread", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				GingerbreadBlock: big.NewInt(0),
+			},
+		}
+		g.initingGenesis = false
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.False(t, b.Header().IsPreGingerbread())
+		g.initingGenesis = true
+		b = g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.False(t, b.Header().IsPreGingerbread())
+	})
+	t.Run("NonMigratedChainWithEthashHasDefaultGasLimit&DifficultySet", func(t *testing.T) {
+		nonMigrateWithEthash := &Genesis{Config: &params.ChainConfig{Ethash: &params.EthashConfig{}}}
+		b := nonMigrateWithEthash.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, params.GenesisGasLimit, b.Header().GasLimit)
+		assert.Equal(t, params.GenesisDifficulty, b.Header().Difficulty)
+	})
+	t.Run("NonMigratedChainWithoutEthashHasDefaultGasLimit&ZeroDifficultySet", func(t *testing.T) {
+		b := nonMigrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, params.GenesisGasLimit, b.Header().GasLimit)
+		assert.Equal(t, new(big.Int), b.Header().Difficulty)
+	})
+	t.Run("MigratedChainGasLimitNotSet", func(t *testing.T) {
+		b := migrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, uint64(0), b.Header().GasLimit)
+	})
+	t.Run("MigratedChainNilDifficultyInitialisedToZero", func(t *testing.T) {
+		b := migrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, big.NewInt(0), b.Header().Difficulty)
+	})
+	t.Run("NonMigratedChainHasEmptyUncleHashSet", func(t *testing.T) {
+		b := nonMigrated.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, types.EmptyUncleHash, b.Header().UncleHash)
+	})
+	t.Run("PreGingerbreadBlocksOnMigratedChainsDoNotHaveEmptyUncleHashSet", func(t *testing.T) {
+		g := &Genesis{
+			Config: &params.ChainConfig{
+				Cel2Time:         &cel2Time,
+				GingerbreadBlock: big.NewInt(1),
+			},
+		}
+		b := g.toBlockWithRoot(common.Hash{}, common.Hash{})
+		assert.Equal(t, common.Hash{}, b.Header().UncleHash)
+	})
+}